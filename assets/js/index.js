764b77fc004fcad0541e83b4d06679f458946e2d
joshcallawaydev <joshrcal89@gmail.com>
2021-05-11 19:30:24 +0000

    Updated game function and added timer.

--- assets/js/index.js
+++ assets/js/index.js
@@ -74,99 +74,107 @@ function stopTimer() {
     seconds = 0;
 }
 
-function startTimer () {
+function startTimer() {
     downloadTimer = setInterval(function () {
         document.getElementById("timer").textContent = seconds++;
-        if (timer === 60){
+        if (timer === 60) {
             stopTimer();
             alert("You have run out of time");
         }
     }, 1000);
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-//define functions 
+function replay() {
+    const name = document.getElementById("fname-index").value;
+    if (!name || name.length < 2) {
+        alert("you must enter your name");
+        return;
+    }
 
-createBoard(grid, cardArray); 
-arrangeCard();
-playAgain.addEventListener("click", replay); 
+    // reset state
+    popup.style.display = "none";
+    clickBoard.innerHTML = "0";
+    cardsWon = 0;
+    clicks = 0;
+    grid.innerHTML = "";
+    startTimer()
+    // Functions
 
-//add a click function for images 
+    createBoard(grid, cardArray);
+    arrangeCard();
 
-imgs = document.querySelectorAll("img");
-Array.from(imgs).forEach(img => 
-img.addEventListener("click", flipCard)
-) 
-});
+    // Adding a click function for images 
 
+    image = document.querySelectorAll("img");
+    Array.from(image).forEach(img =>
+        img.addEventListener("click", flipCard)
+    )
+}
 //createBoard function
 
-function createBoard(grid, array) { 
-popup.style.display = "none"; 
-array.forEach((arr, index) => { 
-let img = document.createElement("img"); 
-img.setAttribute("src", "assets/images/Pokemon_Card.jpeg");
-img.setAttribute("data-id", index); 
-grid.appendChild(img); 
-})
+function createBoard(grid, array) {
+    array.forEach((arr, index) => {
+        let img = document.createElement("img");
+        img.setAttribute("src", "assets/images/Pokemon_Card.jpeg");
+        img.setAttribute("data-id", index);
+        grid.appendChild(img);
+    })
 }
 
 // arrangeCard function
 
-function arrangeCard() { 
-cardArray.sort(() => 0.5 - Math.random())
+function arrangeCard() {
+    cardArray.sort(() => 0.5 - Math.random())
 }
 
 // flip Card function
 
-function flipCard() { 
-let selected = this.dataset.id;
-cardsSelected.push(cardArray[selected].name); 
-cardsId.push(selected); 
-this.classList.add("flip"); 
-this.setAttribute("src", cardArray[selected].img); 
-if (cardsId.length === 2) { 
-setTimeout(checkForMatch, 500);
-} 
-}
+function flipCard() {
+    let selected = this.dataset.id;
+    let clicked = cardArray[selected].name
+    cardsSelected.push(clicked);
 
+
+    cardsId.push(selected);
+    this.classList.add("flip");
+    this.setAttribute("src", cardArray[selected].img);
+    if (cardsId.length === 2) {
+        setTimeout(checkForMatch, 500);
+    }
+}
 // checkForMatch function
 
-function checkForMatch() { 
-let imgs = document.querySelectorAll("img"); 
-let firstCard = cardsId[0];
-let secondCard = cardsId[1];
-if (cardsSelected[0] === cardsSelected[1] && firstCard !== secondCard) { 
-alert("you have found a match"); 
-cardsWon += 1; 
-scoreBoard.innerHTML = cardsWon; 
-setTimeout(checkWon,500) 
-} else { 
-imgs[firstCard].setAttribute("src", "blank.png");
-imgs[secondCard].setAttribute("src", "blank.png"); alert("wrong, please try again"); imgs[firstCard].classList.remove("flip"); imgs[secondCard].classList.remove("flip"); 
-} 
-cardsSelected = []; 
-cardsId = []; 
-clicks += 1; 
-clickBoard.innerHTML = clicks; 
+function checkForMatch() {
+    let imgs = document.querySelectorAll("img");
+    let firstCard = cardsId[0];
+    let secondCard = cardsId[1];
+    if (cardsSelected[0] === cardsSelected[1] && firstCard !== secondCard) {
+
+        cardsWon += 1;
+        setTimeout(checkWon, 500)
+    } else {
+        imgs[firstCard].setAttribute("src", "assets/images/Pokemon_Card.jpeg");
+        imgs[secondCard].setAttribute("src", "assets/images/Pokemon_Card.jpeg");
+        imgs[firstCard].classList.remove("flip");
+        imgs[secondCard].classList.remove("flip");
+    }
+    cardsSelected = [];
+    cardsId = [];
+    clicks += 1;
+    clickBoard.innerHTML = clicks;
 }
 
 function checkWon() {
-if (cardsWon == cardArray.length / 2) {
-alert("You won") 
-setTimeout(()=> popup.style.display = "flex" ,300); 
-}
-}
+    if (cardsWon == cardArray.length / 2) {
+        // const name = document.querySelector("the name element").value;
+        const score = seconds;
+        personalBest.saveData(score, new Date().toUTCString());
 
-// The replay function
-
-function replay() { 
-arrangeCard(); 
-grid.innerHTML = "";
-createBoard(grid, cardArray);
-cardsWon = 0;
-clicks = 0; 
-clickBoard.innerHTML = 0; 
-scoreBoard.innerHTML = 0; 
-popup.style.display = "none"; 
+        stopTimer();
+        alert("You won")
+        // stop timer
+        // save
+        // render personal best
+        setTimeout(() => popup.style.display = "flex", 300);
+    }
 }
