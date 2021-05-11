06f9d80bfa57c800c55d1f5d496b124f5c13dcb1
joshcallawaydev <joshrcal89@gmail.com>
2021-05-11 18:33:33 +0000

    Added js game info button.

---
+++ assets/js/button.js
@@ -0,0 +1,9 @@
+// Interactive button to present the game instructions
+
+function gameInfo() {
+    let info = document.getElementById("gameInfo");
+  if (info.style.display === "none") {
+    info.style.display = "block";
+  } else {
+    info.style.display = "none";
+  }}
